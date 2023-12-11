a6c4c69c0d61b442a956c78103bcc059538dc4df
post@albertvanandel.nl <you@example.com>
2023-12-11 23:55:53 +0100

    day11 prettier

--- day11/galaxies_big.cpp
+++ day11/galaxies_big.cpp
@@ -1,19 +1,22 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-#define GALAXY_AGE 999999
+
+
+#define GALAXY_AGE 100
 
 int main(int argc, char **argv)
 {
 	ifstream file;
-	string line, route;
+	string line;
 	vector<string> universe;
-	bool	all_dots;
 	vector<pair<int, int> > galaxies;
 	vector<int> empty_rows, empty_cols;
-	int	lines = 0, extra_lines, x_dist, y_dist;
+	int	lines = 0, extra_lines, x_start, x_end, y_start, y_end;
 	long distances = 0;
+	bool	all_dots;
 
 
 	if (argc != 2)
@@ -42,39 +45,20 @@ int main(int argc, char **argv)
 				empty_cols.push_back(i);
 		}
 		for (int i = 0; i < universe.size(); i++)
-		{
 			for(int j = 0; j < universe[i].size(); j++)
-			{
 				if (universe[i][j] == '#')
 					galaxies.push_back({i, j});
-			}
-		}
 		for (int i = 0; i < galaxies.size(); i++)
 		{
 			for(int j = 1; j < galaxies.size(); j++)
 			{
 				if (i != j && j > i)
 				{
-					int	x_start, x_end, y_start, y_end;
-					if (galaxies[i].first < galaxies[j].first)
-					{
-						x_start = galaxies[i].first;
-						x_end = galaxies[j].first;
-					} else {
-						x_start = galaxies[j].first;
-						x_end = galaxies[i].first;
-					}
-					if (galaxies[i].second < galaxies[j].second)
-					{
-						y_start = galaxies[i].second;
-						y_end = galaxies[j].second;
-					} else {
-						y_start = galaxies[j].second;
-						y_end = galaxies[i].second;
-					}
 					
-					x_dist = x_end - x_start;
-					y_dist = y_end - y_start;
+					x_start = min(galaxies[i].first, galaxies[j].first);
+					x_end = max(galaxies[i].first, galaxies[j].first);
+					y_start = min(galaxies[i].second, galaxies[j].second);
+					y_end = max(galaxies[i].second, galaxies[j].second);
 					extra_lines = 0;
 					for (int x = x_start; x <= x_end; x++)
 						if (find(empty_rows.begin(), empty_rows.end(), x) != empty_rows.end())
@@ -82,7 +66,7 @@ int main(int argc, char **argv)
 					for (int y = y_start; y <= y_end; y++)
 						if (find(empty_cols.begin(), empty_cols.end(), y) != empty_cols.end())
 							extra_lines++;
-					distances += x_dist + y_dist + extra_lines * GALAXY_AGE;
+					distances += x_end - x_start + y_end - y_start + extra_lines * (GALAXY_AGE - 1);
 				}
 			}
 			
